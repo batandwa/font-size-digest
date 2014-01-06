4f4d039bac9629c885da00c1087c288f56ffc926
Batandwa Colani <i@batandwa.me>
2014-01-06 09:18:09 +0200

    Added function to fetch text DOM elements.

--- fontsizes.js
+++ fontsizes.js
@@ -1,24 +1,48 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
-
 var sizes = [];
+var junk = [];
+var types = [];
 jQuery("body, body *").each(function(a,b) {
+    console.log(b.nodeType);
     // Get the size
     size = jQuery(b).css("font-size");
-    // Remove unit
-    //newsize = size.replace(/[^\d\.]+/, "");
-    //console.log(Math.round(newsize * Math.pow(10, 0.1)) / Math.pow(10, 0.1));
     
+    text = jQuery(b).contents().filter(function() {
+        return this.nodeType == 3;
+    }).text();
+//    console.log(text);
+//    console.log(typeof(text));
+  //      types[b.nodeType] = typeof(types[b.nodeType]) === "undefined" ? [] : types[b.nodeType];
+//        types[b.nodeType].push(b);
+        
+    if(text != "") {
+        sizes[size] = typeof(sizes[size]) === "undefined" ? [] : sizes[size];
+        sizes[size].push(b);
+    } else {
+        junk[size] = typeof(junk[size]) === "undefined" ? [] : junk[size];
+        junk[size].push(b);
 
-    if(typeof(sizes[size]) === "undefined") {
-        sizes[size] = [];
     }
-    sizes[size].push(b);
 })
+
 console.log(sizes);
+console.log(junk);
+console.log(types);
+
+function textNodes() {
+    var all = document.getElementsByTagName("*");
+    var textNodes = [];
+    
+    for (var i=0, max=all.length; i < max; i++) {
+         // Do something with the element here
+         //console.log(all[i].nodeType);
+         //console.log(all[i]);
+         for(var j=0, childCount=all[i].childNodes.length; j<childCount; j++) {
+             if(all[i].childNodes[j].nodeType == 3 && all[i].childNodes[j].nodeValue.trim() != "") {
+                textNodes.push(all[i]);
+                break;
+             }
+         }
+    }
+    
+    return textNodes;
+}
