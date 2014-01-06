9d0563b645a783283a6abc0244acf93b337415f1
Batandwa Colani <i@batandwa.me>
2014-01-06 16:33:35 +0200

    Fixed indentation.

--- fontsizes.js
+++ fontsizes.js
@@ -10,15 +10,15 @@ console.log(sizes);
 function textNodes() {
   var all = document.getElementsByTagName("*");
   var textNodes = [];
-  
+
   for (var i=0, max=all.length; i < max; i++) {
-   for(var j=0, childCount=all[i].childNodes.length; j<childCount; j++) {
-     if(all[i].childNodes[j].nodeType == 3 && all[i].childNodes[j].nodeValue.trim() != "") {
-      textNodes.push(all[i]);
-      break;
+    for(var j=0, childCount=all[i].childNodes.length; j<childCount; j++) {
+      if(all[i].childNodes[j].nodeType == 3 && all[i].childNodes[j].nodeValue.trim() != "") {
+        textNodes.push(all[i]);
+        break;
+      }
     }
   }
-}
 
-return textNodes;
+  return textNodes;
 }
